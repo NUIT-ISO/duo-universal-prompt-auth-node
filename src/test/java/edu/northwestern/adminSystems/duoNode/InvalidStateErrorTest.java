fef0a6da38db4a729842f4273d9a98f1f9283fd5
Nick Evans <nick.evans@northwestern.edu>
2021-05-24 13:45:03 -0500

    Revert "Fail on purpose to see annotations"
    This reverts commit fb082f7b0c188fb3b71d08bc105b0c528b16ba99.

--- src/test/java/edu/northwestern/adminSystems/duoNode/InvalidStateErrorTest.java
+++ src/test/java/edu/northwestern/adminSystems/duoNode/InvalidStateErrorTest.java
@@ -12,7 +12,6 @@ public class InvalidStateErrorTest {
         InvalidStateError e = new InvalidStateError(message);
 
         assertEquals(message, e.getMessage());
-        assertEquals(1, 2);
     }
 
 }
