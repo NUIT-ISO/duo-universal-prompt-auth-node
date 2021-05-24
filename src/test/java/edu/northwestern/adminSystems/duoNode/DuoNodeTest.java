526d620729b99df94ffddbac1353bef2ea5f7318
Nick Evans <nick.evans@northwestern.edu>
2021-05-24 12:54:55 -0500

    More test stubs

---
+++ src/test/java/edu/northwestern/adminSystems/duoNode/DuoNodeTest.java
@@ -0,0 +1,5 @@
+package edu.northwestern.adminSystems.duoNode;
+
+public class DuoNodeTest {
+    // @TODO. awful lotta stuff to mock & stub.
+}
---
+++ src/test/java/edu/northwestern/adminSystems/duoNode/InvalidStateErrorTest.java
@@ -0,0 +1,17 @@
+package edu.northwestern.adminSystems.duoNode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InvalidStateErrorTest {
+    @Test
+    void constructs()
+    {
+        String message = "Test message";
+        InvalidStateError e = new InvalidStateError(message);
+
+        assertEquals(message, e.getMessage());
+    }
+
+}
