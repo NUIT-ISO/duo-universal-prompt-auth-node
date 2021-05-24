6ec8753a9e001b5d3dcfad643fc3e6972faac837
Nick Evans <nick.evans@northwestern.edu>
2021-05-24 11:21:52 -0500

    Implement Duo

---
+++ src/main/java/edu/northwestern/adminSystems/duoNode/DuoConstants.java
@@ -0,0 +1,20 @@
+package edu.northwestern.adminSystems.duoNode;
+
+final class DuoConstants {
+    /**
+     * Key for state param kept in OpenAM's shared state.
+     */
+    static final String SESSION_STATE = "state";
+
+    /**
+     * Response param from Duo
+     */
+    static final String RESP_DUO_CODE = "duo_code";
+
+    /**
+     * Response param from Duo
+     */
+    static final String RESP_STATE = "state";
+
+    static final String DUO_TOKEN_SUCCESSFUL_RESULT = "ALLOW";
+}
--- src/main/java/edu/northwestern/adminSystems/duoNode/DuoNode.java
+++ src/main/java/edu/northwestern/adminSystems/duoNode/DuoNode.java
@@ -1,7 +1,13 @@
 package edu.northwestern.adminSystems.duoNode;
 
+import com.duosecurity.Client;
+import com.duosecurity.exception.DuoException;
+import com.duosecurity.model.Token;
 import com.google.inject.assistedinject.Assisted;
-import org.apache.commons.lang.NotImplementedException;
+import com.iplanet.am.util.SystemProperties;
+import com.sun.identity.authentication.spi.RedirectCallback;
+import com.sun.identity.shared.Constants;
+import org.forgerock.json.JsonValue;
 import org.forgerock.openam.annotations.sm.Attribute;
 import org.forgerock.openam.auth.node.api.AbstractDecisionNode;
 import org.forgerock.openam.auth.node.api.*;
@@ -10,6 +16,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.util.List;
+import java.util.Map;
 
 @Node.Metadata(
         outcomeProvider = AbstractDecisionNode.OutcomeProvider.class,
@@ -17,9 +25,12 @@ import javax.inject.Inject;
 )
 public class DuoNode extends AbstractDecisionNode {
     private final Logger logger = LoggerFactory.getLogger("amAuth");
+
+    private Client duoClient;
     private String clientId;
     private String clientSecret;
     private String apiHostName;
+    private String callbackUri;
     private String failureMode;
 
     @Inject
@@ -27,28 +38,111 @@ public class DuoNode extends AbstractDecisionNode {
         clientId = config.clientId();
         clientSecret = config.clientSecret();
         apiHostName = config.apiHostName();
+        callbackUri = config.callbackUri();
         failureMode = config.failureMode();
+        duoClient = initializeDuoClient();
 
         // @TODO remove me once this is shown to work. but may be helpful for the 1st time we try this node out.
         logger.debug(
-            "Initialized Duo node with the following config: clientID = {}, secret = {} characters, API hostname = {}, and failure mode = {}",
+            "Initialized Duo node with the following config: clientID = {}, secret = {} characters, API hostname = {}, callback URI = {}, and failure mode = {}",
             clientId,
             clientSecret.length(),
             apiHostName,
+            callbackUri,
             failureMode
         );
     }
 
     @Override
     public Action process(TreeContext context) throws NodeProcessException {
-        throw new NotImplementedException();
+        Map<String, List<String>> parameters = context.request.parameters;
+        JsonValue sharedState = context.sharedState;
+        String userReference = sharedState.get(SharedStateConstants.USERNAME).asString().toLowerCase();
+
+        try {
+            duoClient.healthCheck();
+        } catch (DuoException e) {
+            if (failureMode.equals(Config.FailureModes.CLOSED)) {
+                throw new NodeProcessException("Duo health check failed. Cannot proceed.", e);
+            } else {
+                logger.error("Duo health check failed, but failure mode is set to open. Bypassing Duo.");
+                return goTo(true).build();
+            }
+        }
+
+        if (parameters.containsKey(DuoConstants.RESP_DUO_CODE) && parameters.containsKey(DuoConstants.RESP_STATE))
+        {
+            if (! sharedState.isDefined(DuoConstants.SESSION_STATE)) {
+                // This shouldn't happen. It means either someone is doing shenanigans, or something has gone awry with the session.
+                // Clear the session out & try again.
+                logger.warn("Detected Duo callback without initialized session. This may be a spoofing attempt (or a timed out session).");
+
+                sharedState.remove(DuoConstants.SESSION_STATE);
+            } else if (! sharedState.get(DuoConstants.SESSION_STATE).toString().equals(parameters.get(DuoConstants.RESP_STATE))) {
+                // This also shouldn't happen -- the state we stored & the state we got back from the redirect URL param ought to match.
+                // Clear the session out & try again.
+                logger.warn("Detected Duo callback with invalid session. This may be a spoofing attempt (or a timed out session).");
+
+                sharedState.remove(DuoConstants.SESSION_STATE);
+            } else {
+                String duoCode = parameters.get(DuoConstants.RESP_DUO_CODE).toString();
+                String state = sharedState.get(DuoConstants.SESSION_STATE).toString();
+
+                Boolean authenticated = validateDuoAuthenticated(duoCode, state);
+                logger.debug("Duo validation result: {}", authenticated); // @TODO remove this line, it'll be spammy. but useful during dev.
+
+                return goTo(authenticated).build();
+            }
+        }
+
+        String state = duoClient.generateState();
+        sharedState.put(DuoConstants.SESSION_STATE, state);
+
+        String duoUrl = createDuoAuthUrl(userReference, state);
+
+        RedirectCallback redirectCallback = new RedirectCallback(duoUrl, null, "GET");
+        redirectCallback.setTrackingCookie(true);
+        
+        return Action.send(redirectCallback).build();
+    }
+
+    private Client initializeDuoClient() throws NodeProcessException {
+        Client.Builder duoClient = new Client.Builder(clientId, clientSecret, apiHostName, callbackUri);
+
+        try {
+            return duoClient.build();
+        } catch (DuoException e) {
+            throw new NodeProcessException("Could not initialize Duo client. This probably indicates invalid configuration for the auth node.", e);
+        }
+    }
+
+    private String createDuoAuthUrl(String userReference, String state) throws NodeProcessException {
+        try {
+            return duoClient.createAuthUrl(userReference, state);
+        } catch (DuoException e) {
+            throw new NodeProcessException("Unable to create Duo authentication URL", e);
+        }
+    }
+
+    private Boolean validateDuoAuthenticated(String duoCode, String state) throws NodeProcessException {
+        try {
+            Token token = duoClient.exchangeAuthorizationCodeFor2FAResult(duoCode, state);
+
+            if (token == null || token.getAuth_result() == null) {
+                return false;
+            }
+
+            return DuoConstants.DUO_TOKEN_SUCCESSFUL_RESULT.equalsIgnoreCase(token.getAuth_result().getStatus());
+        } catch (DuoException e) {
+            throw new NodeProcessException("Unable to exchange authorization code for result", e);
+        }
     }
 
     /**
      * Configuration for the Duo node.
      */
     public interface Config {
-        public enum FailureModes {
+        enum FailureModes {
             CLOSED,
             OPEN,
         }
@@ -71,6 +165,20 @@ public class DuoNode extends AbstractDecisionNode {
         @Attribute(name = "Failure Mode When Duo is Down", order = 400) // @TODO add choiceValuesClass key & restrict this to CLOSED/OPEN
         default String failureMode() {
             return FailureModes.CLOSED.toString();
+        }
+
+        @Attribute(name = "OpenAM Callback URL", order = 500)
+        default String callbackUri() {
+            final String protocol = SystemProperties.get(Constants.AM_SERVER_PROTOCOL);
+            final String host = SystemProperties.get(Constants.AM_SERVER_HOST);
+            final String port = SystemProperties.get(Constants.AM_SERVER_PORT);
+            final String descriptor = SystemProperties.get(Constants.AM_SERVICES_DEPLOYMENT_DESCRIPTOR);
+
+            if (protocol != null && host != null && port != null && descriptor != null) {
+                return protocol + "://" + host + ":" + port + descriptor;
+            } else {
+                return "";
+            }
         }
 
     }
